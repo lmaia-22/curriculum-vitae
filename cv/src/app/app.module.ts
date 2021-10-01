d8d330309b604f634d55712415e6a1a64e669e33
lmaia-22 <47107328+lmaia-22@users.noreply.github.com>
2021-10-01 18:00:33 +0100

    layout

--- cv/src/app/app.module.ts
+++ cv/src/app/app.module.ts
@@ -7,10 +7,14 @@ import {NgParticlesModule} from "ng-particles";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './layout/header/header.component';
+import { FooterComponent } from './layout/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
